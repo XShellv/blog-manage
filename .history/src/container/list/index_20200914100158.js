3fa42099d7b5d6420a3b95944366519de786a9e3
xiaowuxu <xiaowuxu@trip.com>
2020-09-14 10:20:55 +0800

    增加其他内容

--- .history/src/container/list/index_20200914100158.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Form, Input, Radio, Tag, Tooltip, List, Table } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
-
-export default () => {
-    return (
-        <Table />
-    )
-}
