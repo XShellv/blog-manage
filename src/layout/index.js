25c27bdcdb1ed786d9d52c2678703845c0b63db5
Xshellv <18217706879@163.com>
2020-09-18 08:10:10 +0800

    dsd

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,22 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Layout, Menu, Card, Breadcrumb } from "antd";
-import "./index.scss"
+import "./index.scss";
 
 const { Header, Content, Footer } = Layout;
+const { SubMenu } = Menu;
 
 export default ({ children }) => {
   return (
     <Layout className="layout">
       <Header>
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["list"]}>
+          <SubMenu key="manage" title="文章管理">
+            <Menu.Item key="list">
+              <Link to="/list">文章列表</Link>
+            </Menu.Item>
+            <Menu.Item key="edit">
+              <Link to="/edit">发布文章</Link>
+            </Menu.Item>
+          </SubMenu>
+          <Menu.Item key="about">
+            <Link to="/about">关于我</Link>
+          </Menu.Item>
         </Menu>
       </Header>
       <Content style={{ padding: "0 50px" }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
           <Breadcrumb.Item>App</Breadcrumb.Item>
