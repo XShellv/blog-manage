34239f31ed74fafdfcb72a7f9b2cfef89167b69e
xiaowuxu <xiaowuxu@trip.com>
2020-11-03 10:23:21 +0800

    react-editor-md替换vditor

--- src/components/markdown/index.js
+++ src/components/markdown/index.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
-import Vditor from "vditor";
-import editormd from "editor.md/editormd";
-import "editor.md/css/editormd.css";
+import MDEditor, { commands, ICommand, TextState, TextApi } from '@uiw/react-md-editor';
+
 import "./index.scss";
 
+
 const Markdown = (props) => {
-  const [value, setValue] = useState("### Hello Editor.md !");
-  //   const triggerChange = (changedValue) => {
-  //     const { onChange } = props;
-  //     if (onChange) {
-  //       onChange(changedValue);
-  //     }
-  //   };
+  const triggerChange = (changedValue) => {
+    const { onChange } = props;
+    if (onChange) {
+      onChange(changedValue);
+    }
+  };
 
   // useEffect(() => {
   //   const vditor = new Vditor("vditor", {
@@ -33,38 +32,37 @@ const Markdown = (props) => {
   //     },
   //   });
   // }, [props.value]);
-
-  useEffect(() => {
-    const editor = editormd("editormd", {
-      width: "100%",
-      // 静态资源文件地址
-      // path: "editormd/lib/",
-      height: 640,
-      path: "../lib/",
-      // markdown: md,
-      codeFold: true,
-      searchReplace: true,
-      saveHTMLToTextarea: true, // 保存HTML到Textarea
-      htmlDecode: "style,script,iframe|on*", // 开启HTML标签解析，为了安全性，默认不开启
-      emoji: true,
-      taskList: true,
-      tex: true,
-      tocm: true, // Using [TOCM]
-      autoLoadModules: false,
-      previewCodeHighlight: true,
-      flowChart: true,
-      sequenceDiagram: true,
-    });
-    console.log(editor);
-  }, []);
+  const title3 = {
+    name: 'title3',
+    keyCommand: 'title3',
+    buttonProps: { 'aria-label': 'Insert title3' },
+    icon: (
+      <svg width="12" height="12" viewBox="0 0 520 520">
+        <path fill="currentColor" d="M15.7083333,468 C7.03242448,468 0,462.030833 0,454.666667 L0,421.333333 C0,413.969167 7.03242448,408 15.7083333,408 L361.291667,408 C369.967576,408 377,413.969167 377,421.333333 L377,454.666667 C377,462.030833 369.967576,468 361.291667,468 L15.7083333,468 Z M21.6666667,366 C9.69989583,366 0,359.831861 0,352.222222 L0,317.777778 C0,310.168139 9.69989583,304 21.6666667,304 L498.333333,304 C510.300104,304 520,310.168139 520,317.777778 L520,352.222222 C520,359.831861 510.300104,366 498.333333,366 L21.6666667,366 Z M136.835938,64 L136.835937,126 L107.25,126 L107.25,251 L40.75,251 L40.75,126 L-5.68434189e-14,126 L-5.68434189e-14,64 L136.835938,64 Z M212,64 L212,251 L161.648438,251 L161.648438,64 L212,64 Z M378,64 L378,126 L343.25,126 L343.25,251 L281.75,251 L281.75,126 L238,126 L238,64 L378,64 Z M449.047619,189.550781 L520,189.550781 L520,251 L405,251 L405,64 L449.047619,64 L449.047619,189.550781 Z" />
+      </svg>
+    ),
+    execute: (state, api) => {
+      let modifyText = `### ${state.selectedText}\n`;
+      if (!state.selectedText) {
+        modifyText = `### `;
+      }
+      api.replaceSelection(modifyText);
+    },
+  };
 
   return (
-    <div id="editormd">
-      <textarea
-        value={value}
-        onChange={() => {}}
-        style={{ display: "none" }}
-      ></textarea>
+    <div className="container">
+      <MDEditor
+        value={props.value}
+        onChange={triggerChange}
+        height={500}
+        // commands={[
+        //   commands.bold, commands.hr, commands.italic, commands.divider, commands.codeEdit, commands.codeLive, commands.codePreview, commands.divider,
+        //   commands.fullscreen,
+        //   // Custom Toolbars
+        //   title3,
+        // ]}
+      />
     </div>
   );
 };
